4fbeb0bbc48747a063cd4f5706758718888071be
kenamaka <ejioforkenneth08@gmail.com>
2026-05-20 10:08:00 +0100

    new commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["images.unsplash.com", "www.pexels.com", "images.freepik.com"],
+  },
 };
 
-export default nextConfig;
+
+module.exports = nextConfig;
