87a823c2618e6f638e04182b7cf71e0ea25a9966
kenamaka <ejioforkenneth08@gmail.com>
2026-05-28 12:35:20 +0100

    new

--- lib/fingerprint.ts
+++ lib/fingerprint.ts
@@ -1,9 +1,39 @@
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
 
 export async function getFingerprint() {
+  // =====================================
+  // 1. CHECK EXISTING DEVICE ID
+  // =====================================
+
+  const existingDeviceId =
+    localStorage.getItem("vote_device_id");
+
+  if (existingDeviceId) {
+    return existingDeviceId;
+  }
+
+  // =====================================
+  // 2. GENERATE FINGERPRINT
+  // =====================================
+
   const fp = await FingerprintJS.load();
 
   const result = await fp.get();
 
-  return result.visitorId;
+  // =====================================
+  // 3. CREATE STRONGER PERSISTENT ID
+  // =====================================
+
+  const deviceId = `${result.visitorId}-${crypto.randomUUID()}`;
+
+  // =====================================
+  // 4. SAVE FOREVER
+  // =====================================
+
+  localStorage.setItem(
+    "vote_device_id",
+    deviceId
+  );
+
+  return deviceId;
 }
