a8ebb58bb272eb1f5fd2299269d7554f43bc221f
kenamaka <ejioforkenneth08@gmail.com>
2026-06-18 16:31:21 +0100

    new

--- app/vote/categories/page.tsx
+++ app/vote/categories/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import CategorySlider from "@/src/components/vote/CategorySlider";
+import VotingCountdown from "@/src/components/vote/VotingCountDown";
 import { CATEGORIES } from "@/src/constants/categories";
 import { useVoteStore } from "@/src/store/vote-store";
 import { useRouter } from "next/navigation";
@@ -15,7 +16,12 @@ export default function CategoriesPage() {
   const setSelectedCategory = useVoteStore(
     (state) => state.setSelectedCategory
   );
+const votingStart = new Date("2026-06-20T23:59:00");
+const now = new Date();
 
+if (now < votingStart) {
+  return <VotingCountdown/>;
+}
   return (
     <CategorySlider
       categories={CATEGORIES}
